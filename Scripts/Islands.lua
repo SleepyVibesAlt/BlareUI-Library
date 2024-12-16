80380bb0a07a8fb3cf49c3af220532fb183e31a8
Tajae Daniels <Tkdaniels2010@gmail.com>
2024-12-16 10:55:29 -0500

    we

--- Scripts/Islands.lua
+++ Scripts/Islands.lua
@@ -5,7 +5,8 @@ local Humanoid = Character:WaitForChild("Humanoid")
 local PlayerName = Player.Name
 local Health = Humanoid.Health
 local MaxHealth = Humanoid.MaxHealth
-local Radius = 10 -- Default radius value
+local Radius = 10
+local TweenService = game:GetService("TweenService")
 
 local win = BlareLib:Create({
     Name = "Islands Script",
@@ -23,9 +24,9 @@ AutoFarmTab:Toggle('Auto Tween', function(v)
 end)
 
 AutoFarmTab:Textbox('Autofarm Radius', function(value)
-    local Radius = tonumber(value)
-    if Radius then
-        Radius = Radius
+    local newRadius = tonumber(value)
+    if newRadius then
+        Radius = newRadius
         print("Radius set to:", Radius)
         BlareLib:CreateNotification("Radius Updated", "Radius set to: " .. Radius, 3)
     else
@@ -67,7 +68,7 @@ AutoFarmTab:Toggle('Auto Fish', function(v)
     end
 end)
 
-AutofarmTab:Toggle('Iron Ore', function(v)
+AutoFarmTab:Toggle('Iron Ore', function(v)
     ironFarm = v
     if ironFarm then
         spawn(function()
@@ -99,10 +100,36 @@ AutofarmTab:Toggle('Iron Ore', function(v)
     end
 end)
 
-AutofarmTab:Toggle('Electrite', function(v)
-    
+AutoFarmTab:Toggle('Electrite', function(v)
+    electriteFarm = v
+    if electriteFarm then
+        spawn(function()
+            while electriteFarm and task.wait() do
+                local electrite = getNil("rockElectrite", "Part")
+                if electrite and Player.Character and Player.Character:FindFirstChild("HumanoidRootPart") then
+                    local distance = (electrite.Position - Player.Character.HumanoidRootPart.Position).Magnitude
+                    if distance > Radius then
+                        local tweenInfo = TweenInfo.new(distance/50, Enum.EasingStyle.Linear)
+                        local tween = TweenService:Create(Player.Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(electrite.Position + Vector3.new(0, 3, 0))})
+                        tween:Play()
+                        tween.Completed:Wait()
+                    end
+                    
+                    local args = {
+                        [1] = {
+                            ["xkpOrfvithbzcvKundjsvoamBnpkqBsXm"] = "\7\240\159\164\163\240\159\164\161\7\n\7\n\7\nkyaxebDphmkcyha",
+                            ["part"] = electrite:FindFirstChild("1"),
+                            ["block"] = electrite,
+                            ["norm"] = Vector3.new(-3459.94384765625, 37.332237243652344, -3543.42919921875),
+                            ["pos"] = Vector3.new(-0.26722002029418945, 0.8833552598953247, -0.3850674331188202)
+                        }
+                    }
+
+                    game:GetService("ReplicatedStorage").rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged.CLIENT_BLOCK_HIT_REQUEST:InvokeServer(unpack(args))
+                end
+            end
+        end)
+    end
 end)
-
-
 
 BlareLib:CreateNotification("Island Script Initiated", "Welcome " .. PlayerName .. "!", 4)
