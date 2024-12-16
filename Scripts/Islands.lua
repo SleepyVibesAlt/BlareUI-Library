822f7d1be16bf97168aa72d2286d555815d2dcf4
Tajae Daniels <Tkdaniels2010@gmail.com>
2024-12-16 11:04:56 -0500

    3

--- Scripts/Islands.lua
+++ Scripts/Islands.lua
@@ -37,4 +37,31 @@ AutoFarmTab:Comment('Lower these for better chance of the anticheat not detectin
 
 AutoFarmTab:Section('Resource Farming')
 
+AutoFarmTab:Toggle('Iron Ore', function(v)
+    IronOre = v
+    while IronOre do
+        if ShouldTween then
+            local ironOre = workspace:FindFirstChild("rockIron")
+            if ironOre then
+                local tweenInfo = TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, 0, false, 0)
+                local tween = TweenService:Create(Character.HumanoidRootPart, tweenInfo, {CFrame = ironOre.CFrame * CFrame.new(0, 3, 0)})
+                tween:Play()
+                tween.Completed:Wait()
+            end
+        end
+        
+        local args = {
+            [1] = {
+                ["xkpOrfvithbzcvKundjsvoamBnpkqBsXm"] = "\7\240\159\164\163\240\159\164\161\7\n\7\n\7\nkyaxebDphmkcyha",
+                ["part"] = workspace:FindFirstChild("rockIron"):FindFirstChild("1"),
+                ["block"] = workspace:FindFirstChild("rockIron"),
+                ["norm"] = Vector3.new(-3459.94384765625, 37.332237243652344, -3543.42919921875),
+                ["pos"] = Vector3.new(-0.26722002029418945, 0.8833552598953247, -0.3850674331188202)
+            }
+        }
+
+        game:GetService("ReplicatedStorage").rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged.CLIENT_BLOCK_HIT_REQUEST:InvokeServer(unpack(args))
+        task.wait(0.1)
+    end
+end)
 BlareLib:CreateNotification("Island Script Initiated", "Welcome " .. PlayerName .. "!", 4)
