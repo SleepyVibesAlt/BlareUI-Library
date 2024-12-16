5afa57ad56d8c2d7abe526b27ca5ec9bd0cd9cf5
Tajae Daniels <Tkdaniels2010@gmail.com>
2024-12-16 11:01:58 -0500

    we

--- Scripts/Islands.lua
+++ Scripts/Islands.lua
@@ -36,37 +36,5 @@ end)
 AutoFarmTab:Comment('Lower these for better chance of the anticheat not detecting')
 
 AutoFarmTab:Section('Resource Farming')
-AutoFarmTab:Toggle('Auto Fish', function(v)
-    autoFish = v
-    if autoFish then
-        spawn(function()
-            while autoFish and task.wait(10) do
-                if not Player.Character or not Player.Character:FindFirstChild("HumanoidRootPart") then continue end
-                
-                local args = {
-                    [1] = "cdf239f8-f9a1-4ca0-86a5-413afe000191",
-                    [2] = {
-                        [1] = {
-                            ["playerLocation"] = Player.Character.HumanoidRootPart.Position,
-                            ["direction"] = Player.Character.HumanoidRootPart.CFrame.LookVector,
-                            ["strength"] = 0.5
-                        }
-                    }
-                }
-                
-                game:GetService("ReplicatedStorage").rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged:FindFirstChild("jtEIwpvoyxfyedcIomzEcuywKczW/rpjmsslmMwtNp"):FireServer(unpack(args))
-                task.wait(1)
-                local args = {
-                    [1] = {
-                        ["success"] = true
-                    }
-                }
-                
-                game:GetService("ReplicatedStorage").rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged:FindFirstChild("jtEIwpvoyxfyedcIomzEcuywKczW/vxdJepVeLTscuViRAjHftcqjvpyomnsl"):FireServer(unpack(args))
-            end
-        end)
-    end
-end)
-
 
 BlareLib:CreateNotification("Island Script Initiated", "Welcome " .. PlayerName .. "!", 4)
