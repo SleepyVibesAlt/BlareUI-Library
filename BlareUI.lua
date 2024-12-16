b15b901fbcae0a89fa6d4a7df7b4d207d349ea13
Tajae Daniels <Tkdaniels2010@gmail.com>
2024-12-16 14:08:13 -0500

    3

--- BlareUI.lua
+++ BlareUI.lua
@@ -77,15 +77,15 @@ function Library:Create(table)
 
     local Toggle = Instance.new("TextButton")
     Toggle.Name = "Show/Hide Blare"
-    Toggle.Size = UDim2.new(0, 180, 0, 40)
+    Toggle.Size = UDim2.new(0, 160, 0, 20)
     Toggle.Position = UDim2.new(0.5, -90, 0, -36)
     Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
     Toggle.BackgroundTransparency = 0.5
     Toggle.Text = "Show/Hide Blare"
     Toggle.TextColor3 = Color3.fromRGB(255, 255, 255)
-    Toggle.TextSize = 14
+    Toggle.TextSize = 12
     Toggle.Parent = dark_UI
-
+    Toggle.Font = Enum.Font.Gotham
     local UICorner15 = Instance.new("UICorner")
     UICorner15.Name = "UICorner2"
     UICorner15.Parent = Toggle
