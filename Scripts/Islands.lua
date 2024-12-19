adfd360887820d48355de82cd5e175d904eca7a4
Tajae Daniels <Tkdaniels2010@gmail.com>
2024-12-19 08:34:33 -0500

    ewe

--- Scripts/Islands.lua
+++ Scripts/Islands.lua
@@ -17,55 +17,269 @@ local win = BlareLib:Create({
     }
 })
 
-local SettingsTab = win:Tab('Settings')
-SettingsTab:Section('Global Settings')
+local PathfindingService = game:GetService("PathfindingService")
+local RunService = game:GetService("RunService")
 
-SettingsTab:Toggle('Auto Goto', function(v)
+-- Create the pathfinding function
+local function createPath(destination)
+    local path = PathfindingService:CreatePath({
+        AgentRadius = 2,
+        AgentHeight = 5,
+        AgentCanJump = true,
+        WaypointSpacing = 4
+    })
+    
+    path:ComputeAsync(Character.HumanoidRootPart.Position, destination)
+    
+    if path.Status == Enum.PathStatus.Success then
+        local waypoints = path:GetWaypoints()
+        
+        -- Visualize waypoints
+        for _, waypoint in ipairs(waypoints) do
+            local sphere = Instance.new("Part")
+            sphere.Shape = Enum.PartType.Ball
+            sphere.Size = Vector3.new(0.5, 0.5, 0.5)
+            sphere.Position = waypoint.Position
+            sphere.Anchored = true
+            sphere.CanCollide = false
+            sphere.Material = Enum.Material.Neon
+            sphere.BrickColor = BrickColor.new("Bright blue")
+            sphere.Parent = workspace
+            
+            task.delay(2, function()
+                sphere:Destroy()
+            end)
+            
+            if waypoint.Action == Enum.PathWaypointAction.Jump then
+                Humanoid.Jump = true
+            end
+            Humanoid:MoveTo(waypoint.Position)
+            Humanoid.MoveToFinished:Wait()
+        end
+        return true
+    end
+    return false
+end
+
+local AutoFarmTab = win:Tab('AutoFarms')
+AutoFarmTab:Section('Setting')
+AutoFarmTab:Toggle('Auto Goto', function(v)
     ShouldGoto = v
 end)
 
-SettingsTab:Textbox('Global Radius', function(value)
+AutoFarmTab:Textbox('Autofarm Radius', function(value)
     local newRadius = tonumber(value)
     if newRadius then
         Radius = newRadius
         print("Radius set to:", Radius)
         BlareLib:CreateNotification("Radius Updated", "Radius set to: " .. Radius, 2)
     else
+        BlareLib:CreateNotification("Invalid Input", "Please enter a number, words don't work!", 3)
+    end
+end)
+
+AutoFarmTab:Section('Resource Farming')
+
+AutoFarmTab:Toggle('Iron Ore', function(v)
+    print("Iron Ore toggled:", v)
+    wait(1)
+    IronOre = v
+    while IronOre do
+        local rocks = workspace:GetDescendants()
+        local closestRock = nil
+        local closestDistance = math.huge
+        
+        for _, rock in pairs(rocks) do
+            if rock.Name == "rockIron" and rock:IsA("Part") then
+                local distance = (rock.Position - Character.HumanoidRootPart.Position).Magnitude
+                if distance <= Radius and distance < closestDistance then
+                    closestRock = rock
+                    closestDistance = distance
+                end
+            end
+        end
+        
+        if closestRock then
+            if ShouldGoto then
+                createPath(closestRock.Position)
+            end
+            local args = {
+                [1] = {
+                    ["xkpOrfvithbzcvKundjsvoamBnpkqBsXm"] = "\7\240\159\164\163\240\159\164\161\7\n\7\n\7\nkyaxebDphmkcyha",
+                    ["part"] = closestRock:FindFirstChild("1"),
+                    ["block"] = closestRock,
+                    ["norm"] = closestRock.Position,
+                    ["pos"] = closestRock.Position - Character.HumanoidRootPart.Position
+                }
+            }
+            
+            game:GetService("ReplicatedStorage").rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged.CLIENT_BLOCK_HIT_REQUEST:InvokeServer(unpack(args))
+        end
+        task.wait(0.2)
+    end
+end)
+
+AutoFarmTab:Toggle('Eletrite Ore', function(v)
+    print("Eletrite Ore toggled:", v)
+    wait(1)
+    EletriteOre = v
+    while EletriteOre do
+        local rocks = workspace:GetDescendants()
+        local closestRock = nil
+        local closestDistance = math.huge
+        
+        for _, rock in pairs(rocks) do
+            if rock.Name == "rockElectrite" and rock:IsA("Part") then
+                local distance = (rock.Position - Character.HumanoidRootPart.Position).Magnitude
+                if distance <= Radius and distance < closestDistance then
+                    closestRock = rock
+                    closestDistance = distance
+                end
+            end
+        end
+        
+        if closestRock then
+            if ShouldGoto then
+                createPath(closestRock.Position)
+            end
+            local args = {
+                [1] = {
+                    ["xkpOrfvithbzcvKundjsvoamBnpkqBsXm"] = "\7\240\159\164\163\240\159\164\161\7\n\7\n\7\nkyaxebDphmkcyha",
+                    ["part"] = closestRock:FindFirstChild("1"),
+                    ["block"] = closestRock,
+                    ["norm"] = closestRock.Position,
+                    ["pos"] = closestRock.Position - Character.HumanoidRootPart.Position
+                }
+            }
+            
+            game:GetService("ReplicatedStorage").rbxts_include.node_modules:FindFirstChild("@rbxts").net.out._NetManaged.CLIENT_BLOCK_HIT_REQUEST:InvokeServer(unpack(args))
+        end
+        task.wait(0.2)
+    end
+end)
+
+local MobFarmTab = win:Tab('Mob Farm')
+
+MobFarmTab:Section('Settings')
+
+local SearchDistance = 300
+
+MobFarmTab:Textbox('Search Distance', function(value)
+    local newDistance = tonumber(value)
+    if newDistance then
+        SearchDistance = newDistance
+        print("Search Distance set to:", SearchDistance)
+        BlareLib:CreateNotification("Distance Updated", "Search Distance set to: " .. SearchDistance, 2)
+    else
         BlareLib:CreateNotification("Invalid Input", "Please enter a number!", 3)
     end
 end)
 
-local AntiStuckEnabled = false
-SettingsTab:Toggle('Anti-Stuck', function(v)
-    AntiStuckEnabled = v
-    if AntiStuckEnabled then
-        spawn(function()
-            while AntiStuckEnabled do
-                local noclipConnection
-                noclipConnection = game:GetService('RunService').Stepped:Connect(function()
-                    if Character and Character:FindFirstChild("Humanoid") then
-                        for _, part in pairs(Character:GetDescendants()) do
-                            if part:IsA("BasePart") then
-                                part.CanCollide = false
-                            end
-                        end
-                    end
-                end)
-                
-                wait(5)
-                
-                if noclipConnection then
-                    noclipConnection:Disconnect()
+MobFarmTab:Comment('Please equip your sword before enabling and also go to the island first. Sometimes the autofarm might bug the gui due to the automatic clicking!')
+MobFarmTab:Section('Slime Island')
+
+MobFarmTab:Toggle('Farm Slimes', function(v)
+    local Slimes = game.workspace.WildernessIsland.Entities
+    SlimeFarm = v
+    while SlimeFarm do
+        local closestSlime = nil
+        local closestDistance = math.huge
+       
+        for _, slime in pairs(Slimes:GetChildren()) do
+            if slime.Name == "slime" and slime:FindFirstChild("Humanoid") and slime.Humanoid.Health > 0 and SlimeFarm then
+                local distance = (slime.HumanoidRootPart.Position - Character.HumanoidRootPart.Position).Magnitude
+                if distance < closestDistance and distance <= SearchDistance then
+                    closestSlime = slime
+                    closestDistance = distance
                 end
-                
-                wait(5)
-                
-                if not AntiStuckEnabled then break end
             end
-        end)
+        end
+       
+        if closestSlime then
+            while closestSlime:FindFirstChild("Humanoid") and closestSlime.Humanoid.Health > 0 and SlimeFarm do
+                Humanoid:MoveTo(closestSlime.HumanoidRootPart.Position)
+                workspace.CurrentCamera.CFrame = CFrame.new(workspace.CurrentCamera.CFrame.Position, closestSlime.HumanoidRootPart.Position)
+               
+                local virtualInput = game:GetService("VirtualInputManager")
+                virtualInput:SendMouseButtonEvent(game.Workspace.CurrentCamera.ViewportSize.X/2, game.Workspace.CurrentCamera.ViewportSize.Y/2, 0, true, game, 1)
+               task.wait(0.2)
+                virtualInput:SendMouseButtonEvent(game.Workspace.CurrentCamera.ViewportSize.X/2, game.Workspace.CurrentCamera.ViewportSize.Y/2, 0, false, game, 1)
+               task.wait(0.2)
+            end
+            print("Slime defeated!")
+        end
+        task.wait(0.1)
     end
 end)
 
-SettingsTab:Comment('Anti-Stuck will toggle noclip every 5 seconds to help prevent getting stuck')
+MobFarmTab:Toggle('Farm slimeKing', function(v)
+    local Slimes = game.workspace.WildernessIsland.Entities
+    SlimeKingFarm = v
+    while SlimeKingFarm do
+        local closestSlime = nil
+        local closestDistance = math.huge
+       
+        for _, slime in pairs(Slimes:GetChildren()) do
+            if slime.Name == "slimeKing" and slime:FindFirstChild("Humanoid") and slime.Humanoid.Health > 0 and SlimeKingFarm then
+                local distance = (slime.HumanoidRootPart.Position - Character.HumanoidRootPart.Position).Magnitude
+                if distance < closestDistance and distance <= SearchDistance then
+                    closestSlime = slime
+                    closestDistance = distance
+                end
+            end
+        end
+       
+        if closestSlime then
+            while closestSlime:FindFirstChild("Humanoid") and closestSlime.Humanoid.Health > 0 and SlimeKingFarm do
+                Humanoid:MoveTo(closestSlime.HumanoidRootPart.Position)
+                workspace.CurrentCamera.CFrame = CFrame.new(workspace.CurrentCamera.CFrame.Position, closestSlime.HumanoidRootPart.Position)
+               
+                local virtualInput = game:GetService("VirtualInputManager")
+                virtualInput:SendMouseButtonEvent(game.Workspace.CurrentCamera.ViewportSize.X/2, game.Workspace.CurrentCamera.ViewportSize.Y/2, 0, true, game, 1)
+               task.wait(0.2)
+                virtualInput:SendMouseButtonEvent(game.Workspace.CurrentCamera.ViewportSize.X/2, game.Workspace.CurrentCamera.ViewportSize.Y/2, 0, false, game, 1)
+               task.wait(0.2)
+            end
+            print("Slime King defeated!")
+        end
+        task.wait(0.1)
+    end
+end)
+
+MobFarmTab:Section('Buffalkor Island')
+
+MobFarmTab:Toggle('Farm buffalkor', function(v)
+    local Entities = game.workspace.WildernessIsland.Entities
+    BuffalkorFarm = v
+    while BuffalkorFarm do
+        local closestBuffalkor = nil
+        local closestDistance = math.huge
+       
+        for _, mob in pairs(Entities:GetChildren()) do
+            if mob.Name == "buffalkor" and mob:FindFirstChild("Humanoid") and mob.Humanoid.Health > 0 and BuffalkorFarm then
+                local distance = (mob.HumanoidRootPart.Position - Character.HumanoidRootPart.Position).Magnitude
+                if distance < closestDistance and distance <= SearchDistance then
+                    closestBuffalkor = mob
+                    closestDistance = distance
+                end
+            end
+        end
+       
+        if closestBuffalkor then
+            while closestBuffalkor:FindFirstChild("Humanoid") and closestBuffalkor.Humanoid.Health > 0 and BuffalkorFarm do
+                Humanoid:MoveTo(closestBuffalkor.HumanoidRootPart.Position)
+                workspace.CurrentCamera.CFrame = CFrame.new(workspace.CurrentCamera.CFrame.Position, closestBuffalkor.HumanoidRootPart.Position)
+               
+                local virtualInput = game:GetService("VirtualInputManager")
+                virtualInput:SendMouseButtonEvent(game.Workspace.CurrentCamera.ViewportSize.X/2, game.Workspace.CurrentCamera.ViewportSize.Y/2, 0, true, game, 1)
+               task.wait(0.2)
+                virtualInput:SendMouseButtonEvent(game.Workspace.CurrentCamera.ViewportSize.X/2, game.Workspace.CurrentCamera.ViewportSize.Y/2, 0, false, game, 1)
+               task.wait(0.2)
+            end
+            print("Buffalkor defeated!")
+        end
+        task.wait(0.1)
+    end
+end)
 
 BlareLib:CreateNotification("Island Script Initiated", "Welcome " .. PlayerName .. ", we hope u enjoy!", 2)
