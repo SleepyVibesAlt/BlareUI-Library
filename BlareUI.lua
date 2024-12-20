ab09f65dd0ec632f141e8bd0a7749b1b180c275a
Tajae Daniels <Tkdaniels2010@gmail.com>
2024-12-20 08:20:15 -0500

    ewe

--- BlareUI.lua
+++ BlareUI.lua
@@ -1126,7 +1126,7 @@ print('===================================')
 print('Welcome to BlareUi-Library')
 print('Library Version 1.0.0')
 print('Executor : '.. executor)
-print('Status : False')
+print('Status : Functional')
 print('===================================')
 
 return Library
